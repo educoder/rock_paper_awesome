ca476517e2835c8936fd9593f48e21d2555b0107
Matt Zukowski <matt@zukowski.ca>
2012-10-05 12:07:29 -0400

    switched convertRGBval back for BlinkM

--- rock_paper_awesome.ino
+++ rock_paper_awesome.ino
@@ -212,8 +212,8 @@ void newGame() {
 byte convertRGBval(byte val) {
   // due to the way my LEDs are wired, my pin values are inverted, so for me 0 == maximum, 255 == minimum
   // get rid of the "255-" for yours to revert to normal usage
-  //return 255-val;
-  return val;
+  return 255-val;
+  //return val;
 }
 
 // immediaely set the given led to the given brightness

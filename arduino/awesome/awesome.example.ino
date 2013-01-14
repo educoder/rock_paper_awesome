b3721f1c3173d70bfeb4752b1de2e2221e9b1e1d
Matt Zukowski <matt@zukowski.ca>
2013-01-14 17:07:40 -0500

    revised example.ino to work with new FSM-free code

--- arduino/awesome/awesome.example.ino
+++ arduino/awesome/awesome.example.ino
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "Button.h"
 #include "RPA.h"
 
@@ -13,11 +12,11 @@
 #define PIN_ROCK 5
 #define PIN_PAPER 7
 #define PIN_SCISSORS 6
-// pin that receives input from the NEW GAME (READY) button
+// pin that receives input from the READY button
 #define PIN_READY 3
 
 // wait this many ms before considering that a button has been pressed again
-#define BUTTON_DEBOUNCE_WAIT 50
+#define BUTTON_DEBOUNCE_WAIT 500
 
 // instantiate a RockPaperAwesome state machine
 RPA rpa = RPA();
@@ -30,164 +29,112 @@ Button scissorsButton = Button(PIN_SCISSORS, BUTTON_PULLUP_INTERNAL, true, BUTTO
 
 Button readyButton = Button(PIN_READY, BUTTON_PULLUP_INTERNAL, true, BUTTON_DEBOUNCE_WAIT);
 
+bool present = false;
 
 /*******************************************************************/
 /** EVENT HANDLERS *************************************************/
 /*******************************************************************/
 
-// when you've requested a connection to the XMPP server
-void on_connect() {
+// triggered when you're present and ready to play a game
+void when_you_are_present() {
   //blink(G, 3, 1000);
+  setLED(G, 50);
 }
 
-// when you're connected to the XMPP chatroom
-void on_connected() {
+// triggered when you go away and are no longer able play the game
+void when_you_are_absent() {
   //blink(G, 3, 1000);
-}
-
-// when you've signaled that we're ready to play a game
-void on_ready() {
-  //blink(G, 3, 1000);
-  setLED(G, 15);
-}
-
-// when the other player has signaled that they're ready to play a game
-void on_remote_ready() {
-  //blink(G, 3, 1000);
-}
-
-// when you have chosen a weapon
-void on_you_choose(String weapon) {
-  if (weapon == "Rock")
-    setLED(R, 255);
-  else if (weapon == "Paper")
-      setLED(G, 255);
-  else if (weapon == "Scissors")
-      setLED(B, 255);
-}
-
-// when the other player has chosen a weapon
-void on_they_choose(String weapon) {
-  //blink(G, 3, 1000);
-}
-
-// when you've won
-void on_you_win() {
-  blink(G, 5, 600);
-}
-
-// when you've lost
-void on_you_lose() {
-  blink(R, 5, 600);
-}
-
-// when you've tied
-void on_tie() {
-  blink(B, 5, 600);
-}
-
-// when you've been disconnected
-void on_disconnected() {
-  // blink(B, 5, 600);
-}
-
-/*******************************************************************/
-/** STATE HANDLERS *************************************************/
-/*******************************************************************/
-
-// when you're offline
-void while_OFFLINE() {
-  setLED(R, 15);
-}
-
-// while connecting to XMPP
-void while_CONNECTING() {
-  //setLED(R, 15);
-}
-
-// while online, waiting for a game to start
-void while_ONLINE() {
   setLED(G, 5);
 }
 
-// while waiting for you to say you're ready
-void while_WAITING_FOR_YOUR_READY() {
-  //setLED(R, 15);
+// triggered when the other player is present and ready to play a game
+void when_opponent_is_present(String opponentName) {
+  //blink(G, 3, 1000);
+  setLED(R, 50);
 }
 
-// while waiting for the other player to say they're ready
-void while_WAITING_FOR_THEIR_READY() {
-  //setLED(R, 15);
+// triggered when the other player goes away and is no longer able to play a game
+void when_opponent_is_absent(String opponentName) {
+  //blink(G, 3, 1000);
+  setLED(G, 5);
 }
 
-// while waiting for both players to choose a weapon
-void while_READY_TO_PLAY() {
-  //setLED(R, 15);
+// triggered when you have chosen a weapon
+void when_you_choose_weapon(RPA::WEAPON weapon) {
+  setRGB(0,0,0);
+  if (weapon == RPA::ROCK)
+    setLED(R, 255);
+  else if (weapon == RPA::PAPER)
+      setLED(G, 255);
+  else if (weapon == RPA::SCISSORS)
+      setLED(B, 255);
 }
 
-// while waiting for you to choose a weapon
-void while_WAITING_FOR_YOUR_CHOICE() {
-  //setLED(R, 15);
+// triggered when another player has chosen a weapon
+void when_opponent_chooses_weapon(RPA::WEAPON weapon) {
+  //blink(G, 3, 1000);
 }
 
-// while waiting for the other player to choose a weapon
-void while_WAITING_FOR_THEIR_CHOICE() {
-  //setLED(R, 15);
+// triggered when you've won
+void when_you_win() {
+  setRGB(0,0,0);
+  blink(G, 5, 600);
 }
 
-// while waiting to find out who won or lost
-void while_WAITING_FOR_RESULT() {
-  //setLED(R, 15);
+// triggered when you've lost
+void when_you_lose() {
+  setRGB(0,0,0);
+  blink(R, 5, 600);
+}
+
+// triggered when you've tied
+void when_tie() {
+  setRGB(0,0,0);
+  blink(B, 5, 600);
+}
+
+// triggered when some sort of serious error occurs (usually requiring a game reset)
+void when_error() {
+  blink(R, 5, 600);
 }
 
 /*******************************************************************/
 /** SETUP AND MAIN LOOP ********************************************/
 /*******************************************************************/
 
+void foo() { Serial.println("overridden foo"); };
+
 void setup() {
   // set up the Arduino hardware
   pinMode(R, OUTPUT);
   pinMode(G, OUTPUT);
   pinMode(B, OUTPUT);
+
   // digitalWrite(PIN_ROCK, LOW);
   // digitalWrite(PIN_PAPER, LOW);
   // digitalWrite(PIN_SCISSORS, LOW);
   // pinMode(PIN_ROCK, INPUT);
   // pinMode(PIN_PAPER, INPUT);
   // pinMode(PIN_SCISSORS, INPUT);
-  setRGB(0,0,0);
+
   Serial.begin(SERIAL_BAUDRATE);
+  setRGB(0,0,0);
 
-  // wire up your event handlers here (all optional)
-  rpa.connect = *on_connect;
-  rpa.connected = *on_connected;
-  rpa.ready = *on_ready;
-  rpa.remote_ready = *on_remote_ready;
-  rpa.you_choose = *on_you_choose;
-  rpa.they_choose = *on_they_choose;
-  rpa.you_win = *on_you_win;
-  rpa.you_lose = *on_you_lose;
-  rpa.tie = *on_tie;
-  rpa.disconnected = *on_disconnected;
-
-  // wire up your state entry handlers here (all optional)
-  rpa.OFFLINE = *while_OFFLINE;
-  rpa.CONNECTING = *while_CONNECTING;
-  rpa.ONLINE = *while_ONLINE;
-  rpa.WAITING_FOR_YOUR_READY = *while_WAITING_FOR_YOUR_READY;
-  rpa.WAITING_FOR_THEIR_READY = *while_WAITING_FOR_THEIR_READY;
-  rpa.READY_TO_PLAY = *while_READY_TO_PLAY;
-  rpa.WAITING_FOR_YOUR_CHOICE = *while_WAITING_FOR_YOUR_CHOICE;
-  rpa.WAITING_FOR_THEIR_CHOICE = *while_WAITING_FOR_THEIR_CHOICE;
-  rpa.WAITING_FOR_RESULT = *while_WAITING_FOR_RESULT;
-
-  // tell Node that the Arduino is ready and wants it to connect to XMPP
-  rpa.say_hello();
+  // tell awesome.js that the Arduino is ready
+  rpa.signal_ready();
+  blink(G, 2, 200);
 }
 
 void loop() {
-  if (readyButton.uniquePress())
-    rpa.bow();
+  if (readyButton.uniquePress()) {
+    if (present) {
+      present = false;
+      rpa.absent();
+    } else {
+      present = true;
+      rpa.present();
+    }
+  }
   
   if (rockButton.uniquePress())
     rpa.choose_ROCK();
@@ -198,19 +145,20 @@ void loop() {
   if (scissorsButton.uniquePress())
     rpa.choose_SCISSORS();
 
+  // must call this to process data from awesome.js
   rpa.check_input_from_serial();
 }
 
-/*******************************************************************/
-/** HELPER FUNCTIONS ***********************************************/
-/*******************************************************************/
+/***********************************************************************/
+/** LED HELPER FUNCTIONS ***********************************************/
+/***********************************************************************/
 
 // does any necessary transformation from a LED value to the value that should be written to the pin (using analogWrite)
 byte convertRGBval(byte val) {
   // due to the way my LEDs are wired, my pin values are inverted, so for me 0 == maximum, 255 == minimum
   // get rid of the "255-" for yours to revert to normal usage
-  return val;
   //return val;
+  return 255-val;
 }
 
 // immediaely set the given led to the given brightness
@@ -225,16 +173,15 @@ void setRGB(byte r, byte g, byte b) {
   setLED(B, b);
 }
 
-void blink(byte pin, byte times, int delayTime) {
+void blink(byte pin, byte times, int delayTime, int maxBrightness) {
   for (int i = 0; i < times; i++) {
-    setLED(pin, 255);
+    setLED(pin, maxBrightness);
     delay(delayTime);
     setLED(pin, 0);
     delay(delayTime / 2);
   }
 }
 
-
-
-
-
+void blink(byte pin, byte times, int delayTime) {
+  blink(pin, times, delayTime, 255);
+}
